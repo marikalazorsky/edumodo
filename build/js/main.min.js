5a19f539e674474a7f0378f9220d684ab62bb8ac
Marina Hiuk <mlazor@mail.ru>
2021-01-01 16:08:32 +0100

    add build version

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -1,15 +1,27 @@
-//let close = function () {
-    a = document.querySelectorAll('.nav-link');
-    //let b = document.querySelector('.navbar-collapse').toggle('.show');
+window.onload = function () {
+    let a = document.querySelectorAll('.nav-link');
 
-    for (let a = 0; a < a.length - 1; a++) {
-        a[i].addEventListener('click', toggleNavbar());
+    function addEventListener () {
+        for (let i = 0; i < a.length; i++) {
+            a[i].addEventListener('click', toggleNavbar);
+            a[i].addEventListener('click', scrollTo);
+        }
     }
     
-//}
-
-//close();
-
-function toggleNavbar() {
-    console.log('test');
+    addEventListener();
+    
+    function toggleNavbar() {
+        document.querySelector('.navbar-collapse').classList.toggle('show');
+    }
+    
+    function scrollTo(e) {
+        e.preventDefault();
+        for (let i = 0; i < a.length; i++) {
+            const blockID = a[i].getAttribute('href');
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    }
 }
